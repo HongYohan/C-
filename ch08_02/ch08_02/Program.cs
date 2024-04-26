8434fd7e7e08bed4428c839c24fa2d6bdacc4d44
홍요한 <130424774+HongYohan@users.noreply.github.com>
2024-04-26 23:36:40 +0900

    Add files via upload

---
+++ ch08_02/ch08_02/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+	static void NextPosition(int x, int y, int vx, int vy, out int rx, out int ry)
+	{
+		rx = x+vx;
+		ry = y+vy;
+	}
+	private static void Main(string[] args)
+	{
+		Console.Write("숫자 입력: ");
+		int output; // Console.ReadLine으로 값을 입력 받는다.
+		bool result = int.TryParse(Console.ReadLine(), out output); 
+		// 해당 입력받은 값을 int.TryParse로 int형으로 변환하고
+		// out 뒤의 변수에 해당값을 입력한다 (형변환이 잘되면 true 아니면 false)
+		if(result)
+		{
+			Console.WriteLine("입력한 숫자: "+output);
+		}
+		else
+		{
+			Console.WriteLine("숫자를 입력해 주세요");
+		}
+		int x = 0;
+		int y = 0;
+		int vx = 1;
+		int vy =1;
+		Console.WriteLine($"현재 좌표: ({x},{y})");
+		NextPosition(x,y,vx,vy, out x, out y); // out으로 rx와 ry의 값을 각각 x와 y에 덮어씌여 받는다
+		Console.WriteLine($"다음 좌표: ({x},{y})");
+	}
+}
---
+++ ch08_04/ch08_04/Program.cs
@@ -0,0 +1,33 @@
+﻿internal class Program
+{
+	struct Point // 구조체는 참조 복사가 아닌 값 복사가 일어나는 구조이다
+	{
+		public int x;
+		public int y;
+		public string testA;
+		public string testB;
+		public Point(int x, int y)
+		{
+			this.x = x;
+			this.y = y;
+			this.testA = "초기화";
+			this.testB = "초기화";
+		}
+		public Point(int x, int y, string test)
+		{
+			this.x = x;
+			this.y = y;
+			this.testA = test;
+			this.testB = test;
+		} 
+
+	}
+	private static void Main(string[] args)
+	{
+		Point point = new Point();
+		Console.WriteLine(point.x);
+		Console.WriteLine(point.y);
+		Console.WriteLine(point.testA);
+		Console.WriteLine(point.testB);
+	}
+}
