ef50df1f2024e1d74bd39402f251d97fe16e41aa
홍요한 <130424774+HongYohan@users.noreply.github.com>
2024-04-26 23:30:41 +0900

    Add files via upload

---
+++ ch07_03/ch07_03/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+	class Parent
+	{
+		public int variable = 273;
+		public Parent() 
+		{
+			Console.WriteLine("부모 생성자");
+		}
+	}
+	class Child : Parent
+	{
+		// 변수 하이딩
+		public string variable = "Hello";
+		public Child()
+		{
+			Console.WriteLine("자식 생성자");
+		}
+	}
+	public static int number = 10;
+	private static void Main(string[] args)
+	{
+		Child child = new Child();
+		// 변수 하이딩,, child클래스에서 가장 가까운 변수를 값으로 함
+		Console.WriteLine(child.variable);
+		// 섀도잉
+		int number = 20;
+		Console.WriteLine(number); //20이 출력된다. 이는 가장 가까운 변수명의 값을 선택하기 때문
+	}
+}
---
+++ ch07_04/ch07_04/Program.cs
@@ -0,0 +1,23 @@
+﻿internal class Program
+{
+	class Parent
+	{
+		public virtual void Method() 
+		{
+			Console.WriteLine("부모의 메서드");
+		}
+	}
+	class Child : Parent
+	{
+		public override void Method() // 자식의 메서드에 override 키워드를 적용
+		{
+			Console.WriteLine("자식의 메서드");
+		}
+	}
+	private static void Main(string[] args)
+	{
+		Child child = new Child();
+		child.Method();
+		((Parent)child).Method(); // 오버라이딩으로 인해 Parent의 값도 child 값으로 변경됨
+	}
+}
