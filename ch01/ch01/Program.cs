14cfc8bf6ca275b51a63c47d3717389f2dea6bb4
홍요한 <130424774+HongYohan@users.noreply.github.com>
2024-04-26 22:03:26 +0900

    Add files via upload

---
+++ ch01/ch01/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args) //main이 클래스안에 들어가있음
+    {
+        Console.WriteLine("Hello, World!"); //항상 마무리로 세미콜론(;)사용
+        Console.Write("Hello, World!"); //Line = \n
+        Console.WriteLine("Hello, World!");
+    }//대부분의 단어 시작은 대문자로 시작함
+}
