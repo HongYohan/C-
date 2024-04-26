8ff68d5d84a0f2c43f23e623a2e7e965f106eb98
홍요한 <130424774+HongYohan@users.noreply.github.com>
2024-04-26 23:33:31 +0900

    Add files via upload

---
+++ ch07_05/ch07_05/Program.cs
@@ -0,0 +1,35 @@
+﻿internal class Program
+{
+	class Animal
+	{
+		public virtual void Eat() {
+			Console.WriteLine("냠냠 먹습니다.");
+		}
+	}
+	class Dog : Animal
+	{
+		public new void Eat() // new로 인해 무조건 부모 클래스의 Eat에 대한 결과값이 나와야 한다
+		{
+			Console.WriteLine("강아지 사료를 먹습니다.");
+		}
+	}
+	class Cat : Animal
+	{
+		public override void Eat() //virtual에 대한 값을 없애고 해당 override에 대한 Eat값을 표기한다.
+		{
+			Console.WriteLine("고양이 사료를 먹습니다.");
+		}
+	}
+	private static void Main(string[] args)
+	{
+		List<Animal> list = new List<Animal>() 
+		{
+			new Dog(),new Cat(),new Cat(),new Dog(),
+			new Dog(),new Cat(),new Dog(),new Dog(),
+		};
+		foreach (var item in list)
+		{
+			item.Eat();
+		}
+	}
+}
---
+++ ch07_06/ch07_06/Program.cs
@@ -0,0 +1,25 @@
+﻿internal class Program
+{
+	abstract class Parent // abstract는 반드시 상속해야 하는 속성이다
+	{
+		public abstract void Test(); // Parent 클래스 안에 abstract가 하나 더있다.
+	}
+	class Child : Parent // class Parent에 대한 상속을 함
+	{
+		public override void Test() { } // 내부 상속인데 여기서는 abstract는 반드시 상속이라는 조건이 있기에
+										// 굳이 virtual을 안쓰고 override만 써도 해당 Test에 대한 override를 한다
+	}
+
+	sealed class Parent1 // sealed를 클래스에 적용하면 상속을 절대 하지말라는 의미
+	{
+		sealed public override void Test1() { }
+	}
+	class Child : Parent // sealed로 인해 상속이 불가능하다
+	{
+		public  void override Test1() { } // sealed로 인해 오버라이드가 불가능 해진다  
+	}
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("Hello, World!");
+	}
+}
