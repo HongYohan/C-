a2dedef850d14545ba4865fab42cc79444e29533
홍요한 <130424774+HongYohan@users.noreply.github.com>
2024-04-26 23:24:53 +0900

    Add files via upload

---
+++ ch06_03/ch06_03/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+	class Product
+	{
+		public static int counter =0;
+		public int id;
+		public string name;
+		public int price;
+		public Product(string name, int price)
+		{
+			Product.counter = counter+1;
+			this.id = counter; // this는 클래스 자기자신을 의미 즉, Product.name하고 같은 의미이다.
+			this.name = name;
+			this.price = price;
+		}
+		public const double PI = 3.141592; // const는 상수 생성 호출어 이다
+		~Product() // ~는 소멸자 호출 기호이다.
+		{
+			Console.WriteLine(this.name+"의 소멸자 호출");
+		}
+	}
+	private static void Main(string[] args)
+	{
+		// Program 변수는 private 접근 제한자 이다,, 접근 제한자는 오직 해당 클래스 내부에서만 호출 가능
+		Product productA = new Product("감자", 2000); // 각 생성자의 값을 초기화 시킨다.
+		Product productB = new Product("고구마", 3000);
+		Console.WriteLine(productA.id + ":" + productA.name);
+		Console.WriteLine(productB.id + ":" + productB.name);
+		Console.WriteLine(Product.counter + "개 생성되었습니다.");
+		GC.Collect();
+	}
+}
---
+++ ch06_04/ch06_04/Program.cs
@@ -0,0 +1,53 @@
+﻿internal class Program
+{
+	class Box
+	{
+		//변수
+		private int width;
+		private int height;
+
+		public int Width
+		{
+			get { return width; } // Getter 은 반드시 return이 있어야 한다.
+			set // 속성 값
+			{
+				// value가 해당 int Width의 값인데 그것에 대한 조건을 맞추고 width에 덮어쓰기 한다
+				if (value > 0) { width = value; } 
+				else { Console.WriteLine("너비는 자연수를 입력해주세요"); }
+			}
+		}
+		
+		public int Height
+		{
+			get { return height; }
+			set
+			{
+				if (value > 0) { height = value; }
+				else { Console.WriteLine("높이는 자연수를 입력해주세요"); }
+			}
+		}
+		public Box(int width, int height)
+		{
+			Width = width;
+			Height = height;
+		}
+
+		public void Area() 
+		{ 
+			if(this.width * this.height == 0) { Console.WriteLine("값을 정확하게 입력하시오"); }
+			else Console.WriteLine(this.width * this.height);
+		}
+	}
+	private static void Main(string[] args)
+	{
+		Console.WriteLine("두 자연수 입력");
+		int a = int.Parse(Console.ReadLine());
+		int b = int.Parse(Console.ReadLine());
+		Box box = new Box(a, b);
+		
+		/*box.Width = 20;
+		box.Height = 10;*/
+
+		box.Area();
+	}
+}
